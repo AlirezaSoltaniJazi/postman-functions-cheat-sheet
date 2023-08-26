902049f5f353c4514fd6f78bc5b5f6cf77e0ce20
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2023-08-26 18:05:15 +0330

    Add timestamp to date

---
+++ timestamp-to-date.js
@@ -0,0 +1,22 @@
+/**
+ * Convert timestamps to formatted dates
+ * @param {...number} timestamps The timestamps to convert
+ * @param {boolean} [verbose=false] Whether to log messages to the console
+ */
+let timestampToDate = (verbose = false, ...timestamps) => {
+    if (verbose) console.log('Timestamp to date started!');
+    timestamps.forEach((timestamp, index) => {
+        const date = new Date(timestamp * 1000);
+        const formattedDate = date.toLocaleDateString('en-GB', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit'
+        }).replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$3/$2/$1');
+        if (verbose) console.info(`Formatted date for index ${index + 1}`, formattedDate);
+    });
+    if (verbose) console.log('Timestamp to date ended!');
+}
+pm.environment.set('function_timestamp_to_Date', timestampToDate.toString())
